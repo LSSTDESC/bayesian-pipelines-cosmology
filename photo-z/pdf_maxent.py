ed37d21298b61b49539abd53fb9ab2ba67816a75
Eleni <eleni.tsaprazi@fysik.su.se>
2022-07-29 12:46:57 +0200

    compactified functions

--- photo-z/pdf_maxent.py
+++ photo-z/pdf_maxent.py
@@ -2,8 +2,10 @@ import sys
 sys.path.append("/Users/eleni/Desktop/pymaxent/")
 
 import numpy as np 
+from scipy.interpolate import interp1d
 
 from pymaxent import *
+
 
 def reconstruct_from_moments(p, x, N):
     
@@ -18,3 +20,16 @@ def reconstruct_from_moments(p, x, N):
     sol, lambdas = reconstruct(moments, bnds=[bnd_low, bnd_high])
 
     return x, sol(x)
+
+
+def run(nzs, zs):
+    sol, xs = [], []
+    N_moments = [9, 9, 6, 5, 8]
+
+    for i in range(len(nzs)):
+        x_0, sol_0 = reconstruct_from_moments(nzs[i](zs), zs, N_moments[i])
+        sol_0 = interp1d(x_0, sol_0, fill_value="extrapolate")
+        xs.append(xs)
+        sol.append(sol_0)
+    return xs, sol
+
